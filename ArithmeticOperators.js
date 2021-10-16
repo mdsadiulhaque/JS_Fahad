4de057afacacf60835dedeca637694776b8b8a8e
Md.Sadiul Haque <sadiulhaquefahad56@gmail.com>
2021-10-17 00:09:44 +0600

    up
    simple math

---
+++ ArithmeticOperators.js
@@ -0,0 +1,14 @@
+let x=10;
+let y=3;
+
+console.log (x+y);
+console.log (x-y);
+console.log (x*y);
+console.log (x/y);
+//increment
+console.log (x++);
+console.log (x);
+
+//derement
+console.log (x--);
+console.log (x);
