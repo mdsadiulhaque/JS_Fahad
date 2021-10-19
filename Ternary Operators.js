cc66194da865bb154e7aa25489e300de6055ddcf
Md.Sadiul Haque <sadiulhaquefahad56@gmail.com>
2021-10-19 13:31:04 +0600

    Update

--- Ternary Operators.js
+++ Ternary Operators.js
@@ -7,9 +7,9 @@ console.log(type);
 let sadiul=100;
 let typec=sadiul>90? 'Green':'Red';
 
+console.log(typec);
 
-console.log(typec);
-console.log(typec);
+
 
 
 
---
+++ nen vollien.js
@@ -0,0 +1,32 @@
+
+//logical AND(&&)
+//return ture if both operaands are true
+console.log(true&&true);
+
+console.log(false&&true);
+
+
+
+
+let highincome=true;
+let goodincome = true;
+let eligiableForLone = highincome && goodincome;
+
+console.log(eligiableForLone);
+
+//logical or(||)
+//Return TRUE if one of the operands is TRUE
+
+let highincome1=true;
+let goodincome2 = true;
+let eligiableForLone1 = highincome || goodincome;
+
+console.log(eligiableForLone1);
+
+
+
+let highincome3=false;
+let goodincome4 = true;
+let eligiableForLone2 = highincome || goodincome;
+
+console.log(eligiableForLone2);
