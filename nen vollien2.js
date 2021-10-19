4234a0e1e61661fbbcc08abc1bcc5dd113e95076
Md.Sadiul Haque <sadiulhaquefahad56@gmail.com>
2021-10-19 13:59:17 +0600

    Update

---
+++ nen vollien2.js
@@ -0,0 +1,15 @@
+
+//falsy(False)
+//underdefine
+//null
+//0
+//false
+//''
+//NaN
+
+let usercolor = 'RED';
+let defultcolor = 'BLUE';
+let currentcolor = usercolor || defultcolor;
+
+
+console.log(currentcolor);
