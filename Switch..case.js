2e317407ed341eea77f52faa9a38993f2e3e26a1
Md.Sadiul Haque <sadiulhaquefahad56@gmail.com>
2021-10-21 01:13:41 +0600

    Update

---
+++ Switch..case.js
@@ -0,0 +1,23 @@
+
+let role;
+
+switch( role ) {
+case 'Guest':
+    console.log.apply('Guest user');
+    brack;
+    
+        case 'modertor':
+            console.log.apply('moderto user');
+            brack;
+
+            default:
+                console.log('Unknow User');
+            
+}
+
+
+if ( role==='guest'&& hour>12)
+console.log( 'guest' );
+else if ( role === 'moderator')
+console.log('moderator');
+else consol . log('Unknow User');
