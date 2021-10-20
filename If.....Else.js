e34396d473be8af040e22441eb0212b57ba37fe6
Md.Sadiul Haque <sadiulhaquefahad56@gmail.com>
2021-10-21 00:58:12 +0600

    Update

---
+++ If.....Else.js
@@ -0,0 +1,18 @@
+
+// hour
+//if hour id between 6am and 12pm.good morning
+//if it is between 12 pmand 6om;good afternoon;
+//otherwish: good evening
+
+let hour=24; 
+if(hour>= 6 && hour < 12){
+console.log('good morning'); 
+
+}
+else if (hour>=12 && hour <18){
+console.log('good afternoon ');
+
+}
+
+    else 
+    console.log('good evening');
