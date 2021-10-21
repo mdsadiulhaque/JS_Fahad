3e3230db0a3ab0b08b2bc67ddb4be77e9fb73c51
Md.Sadiul Haque <sadiulhaquefahad56@gmail.com>
2021-10-21 23:37:12 +0600

    Update

---
+++ For....loop.js
@@ -0,0 +1,15 @@
+
+
+for ( let i =0; i<=5; i++){
+
+    console.log('hello word',i);
+
+    if (i % 2 !==0)
+    console.log(i);
+
+}
+
+for (let j=10;j>=1;j--){
+console.log('hey FAHAD WHATS UP',j);
+
+}
