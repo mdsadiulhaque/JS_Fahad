aa939476d7f0b78af2328602b069411fd7e7b033
Md.Sadiul Haque <sadiulhaquefahad56@gmail.com>
2021-10-22 00:21:45 +0600

    Update

---
+++ For.....of.Loop.js
@@ -0,0 +1,33 @@
+
+
+
+//for
+//while
+//do-while
+
+ 
+// for-in
+
+const person = {
+    name:'FAHAD',
+    age:18
+    
+    }; 
+    
+    for ( let key in person  )
+    
+    console.log(key, person[key]);
+    
+    
+    const colors = ['red','green','blue'];
+    
+    for ( let index in colors)
+     console.log(index, colors[index]);
+    
+
+     // for of
+
+     for (let color of colors)
+
+     console.log(color);
+     
