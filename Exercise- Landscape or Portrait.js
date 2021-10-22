fddaedb0038ac36266192ed344e6db8ed69835a6
Md.Sadiul Haque <sadiulhaquefahad56@gmail.com>
2021-10-22 23:31:20 +0600

    Update

---
+++ Exercise- Landscape or Portrait.js
@@ -0,0 +1,14 @@
+
+console.log(isLandscape (800,600));
+
+function isLandscape(width,height) {
+
+/*if(width > height)
+return true;
+return false;
+ */
+
+
+return(width > height);
+
+}
--- Max of Two Number.js
+++ Max of Two Number.js
@@ -11,3 +11,4 @@ return b;*/
 
 return (a>b)? a:b;
  }
+ 
