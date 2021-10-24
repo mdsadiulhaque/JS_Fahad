4d8cb6d5dd9ffcf8ead53e272a3c6c123a2cd264
Md.Sadiul Haque <sadiulhaquefahad56@gmail.com>
2021-10-24 23:22:17 +0600

    Update

--- Exercise- Demerit Points.js
+++ Exercise- Demerit Points.js
@@ -1,5 +1,25 @@
    
+// Math.floor (1.3)
+// checkSpeed(50);
+checkSpeed(80);
+function checkSpeed(Speed){
+    const SpeedLimit=70;
+    const kmPerPoint=5;
 
-    checkSpeed(50);
+    if (Speed < SpeedLimit)
+
+    console.log('Ok');
+else{
+     const point=Math.floor((Speed-SpeedLimit)/kmPerPoint);
+    if (points>=12)
+console.log('License suspended');
+else
+console.log('Point',points )
+
+}
+
+}
+
+
 
     
