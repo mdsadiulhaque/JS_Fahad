32d9e63404f7f9cd2a0fbb580df95e657ce12439
Md.Sadiul Haque <sadiulhaquefahad56@gmail.com>
2021-10-25 23:51:46 +0600

    Update

---
+++ grade.js
@@ -0,0 +1,26 @@
+
+
+const marks =[80,80,50];
+
+console.log(calculateGrade(marks));
+
+
+document.getElementById("demo").innerHTML=calculateGrade(marks);
+function calculateGrade(marks){
+
+    const average =calculateGrade (marks);
+    if (average<60)return'F';
+    if (average<70)return'D';
+    if (average<80)return'C';
+    if (average<90)return'B';
+   return'A';
+
+}
+
+function calculateGrade(array){
+let sum =0;
+for (let value of array)
+sum +=value;
+return sum/array.length;
+
+}
