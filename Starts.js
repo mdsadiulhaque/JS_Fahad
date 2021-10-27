ed3c8fdf0e90ff396677effdafdcef7905c254bd
Md.Sadiul Haque <sadiulhaquefahad56@gmail.com>
2021-10-27 23:06:46 +0600

    Update

--- Starts.js
+++ Starts.js
@@ -2,10 +2,17 @@
 
 showStars(10);
 function showStars(rows){
-for (let row =1; row<=rows; row++)
+for (let row =1; row<=rows; row++){
 
-let pattern ='';
+let pattern =' ';
 for(let i=0; i<row; i++)
  pattern +='*';
 console.log(pattern);
 }
+}
+
+
+// var a=2,b=3,c;
+// c = a+b;
+// console.log(c);
+// document.getElementById("h1").innerHTML=c;
